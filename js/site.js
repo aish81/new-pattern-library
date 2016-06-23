57ab1ee9645599502b67c9cb072361e672e7a1b2
ashleigh.henry <ashleighhenry@outlook.com>
2016-06-23 16:20:39 +0100

    bem naming started on components.html

--- js/site.js
+++ js/site.js
@@ -3,24 +3,20 @@
  */
 console.log("loaded");
 $ (function(){
-    console.log("ready");
     $('.mobile-nav-items, .search-input-holder').hide();
     $('.menu-button-open').on('click',function(){
         $('.mobile-nav-items').slideToggle();
     })
 
-    $('.search-item-open').on('click',function(){
+    $('.nav-search-item-open').on('click',function(){
         $('.search-input-holder').slideToggle();
     })
 
-
     $('.main-input-search-trigger').on('click',function(){
-        $('.full-search').css('display','block').animate({left:0});
+        $('.nav-main-nav-full-search').css('display','block').animate({left:0});
     })
 
     $('.search-close').on('click',function(){
-        $('.full-search').animate({left:'100%'}).fadeOut('slow');
+        $('.nav-main-nav-full-search').animate({left:'100%'}).fadeOut('slow');
     })
-
-
 });
